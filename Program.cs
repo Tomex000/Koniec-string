5149342bb9eade0bc80a1f29fe455d881602b565
Tomex000 <39013545+Tomex000@users.noreply.github.com>
2018-06-25 09:50:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stop="a";
+            
+            while(stop!="koniec")
+            {
+                stop = Console.ReadLine();
+                Console.WriteLine(stop);
+            }
+           
+
+        }
+    }    
+}
